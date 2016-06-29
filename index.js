31b54c4c661d299b4c2633e2bbd5b80c8ae2fa5a
Gerard Wilson <lucid7net@gmail.com>
2016-06-29 17:33:59 +1000

    todays committ

--- index.js
+++ index.js
@@ -17,24 +17,33 @@ var Botkit = require('botkit');
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
   token: process.env.SLACKBOT_TOKEN
-})
+});
 bot.startRTM(function(error, whichBot, payload) {
   if (error) {
     throw new Error('Could not connect to Slack');
   }
 });
 
+//var userList = () => createArray.user.list;
+
 controller.hears(['hello'], ['mention'], function(whichBot, message) {
   whichBot.reply(message, 'Oh Hai! Did you say soomething?' + Date());
 });
 
-controller.hears(['hey'], ['mention'], function(whichBot, message) {
+controller.hears(['hey' || 'howdy'], ['mention'], function(whichBot, message) {
   whichBot.reply(message, 'Hey yourself, do you want to use Nicknames?' + Date());
 });
+
 
 
 
 /*controller.hears(['name'], ['mention'], function(whichBot, message) {
   whichBot.reply(message, "Hi, what's your name?");
 });*/
-
+/*
+1 respond to hello
+2 respond  to informal greeting with invite to use nickname
+3 give user a nickname or can I get them to choose a nickname from a list?
+4 generate a list of users
+5 Notify me whenever my name is on a list i.e. whenever a user.list has been generated
+*/
